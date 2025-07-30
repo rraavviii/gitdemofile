7bea292d3c2aa17907b46f354424c28efeb4cee3
Ravi Kumar <ravikumar16208062@gmail.com>
2025-07-30 14:46:14 +0530

    Saying Hello to Samarth

--- gitdemo.java
+++ gitdemo.java
@@ -2,4 +2,8 @@ public class gitdemo {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+	
+	public void HelloSamarth(){
+	System.out.println("Hello Samarth");
+}
 }
